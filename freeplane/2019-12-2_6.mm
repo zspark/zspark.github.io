00ce02a50e2fa50accc0cee191eb1296ee795908
zhaowei1 <zhaowei1@sensetime.com>
2019-12-06 20:26:31 +0800

    my work

---
+++ freeplane/2019-12-2_6.mm
@@ -0,0 +1,196 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1574741192894"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <b><font size="4">2019-11-25_29</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook NAME="MapStyle" zoom="1.331">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8"/>
+<node TEXT="monday" POSITION="right" ID="ID_1118692209" CREATED="1574667432887" MODIFIED="1574741209262">
+<edge COLOR="#ff0000"/>
+<node TEXT="what to do" ID="ID_1273657940" CREATED="1574741758816" MODIFIED="1574741760566">
+<node TEXT="&#x7ee7;&#x7eed;&#x6574;&#x7406;&#xff35;&#xff2d;&#xff2c;&#x7c7b;&#x56fe;" ID="ID_210424181" CREATED="1575251990573" MODIFIED="1575270192557">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="&#x5b66;&#x4e60;&#x7406;&#x89e3;&#x5176;&#x6d41;&#x7a0b;" ID="ID_1388879762" CREATED="1575270195452" MODIFIED="1575340254501">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node TEXT="what got" ID="ID_3681812" CREATED="1574741761097" MODIFIED="1574824355533">
+<node TEXT="&#x4ea7;&#x54c1;&#x8bb2;&#x8ff0;&#x81ea;&#x5df1;&#x7684;&#x603b;&#x7ed3;&#x5f88;&#x5feb;&#xff0c;&#x96be;&#x9053;&#x662f;&#x7ed9;&#x5173;&#x6ce8;&#x6b64;&#x4efb;&#x52a1;&#x7684;&#x540c;&#x4e8b;&#x8bf4;&#x7684;&#xff1f;&#x81f3;&#x5c11;&#x6211;&#x6ca1;&#x8ddf;&#x4e0a;&#xff0c;&#x53ef;&#x80fd;&#x6211;&#x662f;&#x65b0;&#x4eba;&#x5427;" ID="ID_535042320" CREATED="1575253855863" MODIFIED="1575253925983"/>
+<node TEXT="Matrix4x4f worldmat = parentmat*localmat;&#xa;&#x8fd9;&#x662f;&#x4e00;&#x4e2a;&#x6811;&#x72b6;&#x7ed3;&#x6784;&#xff0c;&#x600e;&#x4e48;&#x4fdd;&#x8bc1;&#x4ec5;&#x4ec5;&#x4e58;&#x4ee5;&#x7236;&#x77e9;&#x9635;&#x5c31;&#x662f;&#x4e16;&#x754c;&#x77e9;&#x9635;&#x7684;&#xff1f;" ID="ID_1877980196" CREATED="1575258733910" MODIFIED="1575258816121">
+<icon BUILTIN="help"/>
+<node TEXT="&#x542c;&#x745e;&#x5065;&#x8bf4;&#x6539;&#x6210;&#x4e86;worldMatrix" ID="ID_534414363" CREATED="1575355778473" MODIFIED="1575355800943"/>
+</node>
+<node TEXT="SkinnedMeshRenderer vs MeshRenderer" ID="ID_530230163" CREATED="1575272753842" MODIFIED="1575276140702">
+<icon BUILTIN="help"/>
+<node TEXT="&#x4e00;&#x4e2a;&#x662f;&#x9759;&#x6001;mesh&#xff0c;&#x4e00;&#x4e2a;&#x662f;&#x9aa8;&#x9abc;&#x52a8;&#x753b;&#x76f8;&#x5173;&#x7684;" ID="ID_546003915" CREATED="1575355805341" MODIFIED="1575355825938"/>
+</node>
+<node TEXT="__TOC1__ __TIC1__" ID="ID_682773356" CREATED="1575276130261" MODIFIED="1575276138894">
+<icon BUILTIN="help"/>
+<node TEXT="&#x4e0d;&#x77e5;&#x9053;&#x5168;&#x540d;&#x662f;&#x4ec0;&#x4e48;" ID="ID_1781047212" CREATED="1575355830501" MODIFIED="1575355839429"/>
+</node>
+<node TEXT="AnimationCurve vs CAnimationCurve" ID="ID_1918158606" CREATED="1575340338092" MODIFIED="1575340348628">
+<icon BUILTIN="help"/>
+<node TEXT="&#x745e;&#x5065;&#x6682;&#x4e0d;&#x8bb0;&#x5f97;&#x4ec0;&#x4e48;&#x533a;&#x522b;&#xff0c;&#x7ec6;&#x7ea0;&#x5e94;&#x8be5;&#x53ef;&#x4ee5;&#x3002;" ID="ID_384190256" CREATED="1575355848644" MODIFIED="1575355916033"/>
+</node>
+</node>
+</node>
+<node TEXT="tuesday" POSITION="right" ID="ID_219882678" CREATED="1574667619591" MODIFIED="1574741219028">
+<edge COLOR="#0000ff"/>
+<node TEXT="what to do" ID="ID_1743820220" CREATED="1574741726773" MODIFIED="1574741734257">
+<node TEXT="&#x6574;&#x7406;st3dengine&#x7684;&#x65f6;&#x5e8f;&#x56fe;" ID="ID_1567552214" CREATED="1575340267321" MODIFIED="1575597161168">
+<icon BUILTIN="button_ok"/>
+<node TEXT="&#x505c;&#x6b62;&#x5168;&#x90e8;&#x6574;&#x7406;&#xff0c;&#xff43;&#x98ce;&#x683c;&#x7684;&#xff21;&#xff30;&#xff29;&#x63a5;&#x53e3;&#xff0c;&#x79cd;&#x7c7b;&#x7e41;&#x591a;&#xff0c;&#x53ef;&#x6574;&#x7406;&#x590d;&#x6742;&#x63a5;&#x53e3;&#xff1b;" ID="ID_1208376409" CREATED="1575355928081" MODIFIED="1575356028545"/>
+</node>
+<node TEXT="&#x8fdb;&#x4e00;&#x6b65;&#x7406;&#x89e3;st3dengine&#x5e93;" ID="ID_534559440" CREATED="1575341931930" MODIFIED="1575597164366">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node TEXT="what got" ID="ID_1383669433" CREATED="1574741782797" MODIFIED="1574741863248">
+<node ID="ID_1755623143" CREATED="1575356500949" MODIFIED="1575356546973"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      AvatarCoreModel??
+    </p>
+    <p>
+      <font size="1" color="#c4aaaa">(utils_avatarmodel.cpp)</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="&#x8bfb;&#x53d6;&#x4e86;&#x7248;&#x672c;&#x53f7;&#xff1f;&#x504f;&#x79fb;&#x4e86;&#x6570;&#x636e;&#x7684;&#x4e00;&#x4e2a;&#x51fd;&#x6570;" ID="ID_573430423" CREATED="1575356672641" MODIFIED="1575356710642"/>
+</node>
+<node TEXT="makeup:&#x80e1;&#x5b50;&#xff0f;&#x816e;&#x7ea2;&#xff0f;&#x773c;&#x7403;&#x7684;&#x989c;&#x8272;&#xff0f;&#x773c;&#x5f71;&#x7b49;" ID="ID_207949426" CREATED="1575359749188" MODIFIED="1575359772964"/>
+<node TEXT="accessory(feature):&#x5934;&#x53d1;&#x7b49;" ID="ID_1239464842" CREATED="1575359773465" MODIFIED="1575360024172"/>
+<node TEXT="&#x591a;&#x4eba;avatar&#x4f1a;&#x8bae;&#xff0c;&#x7409;&#x7483;&#x5382;&#x4f1a;&#x8bae;&#x5ba4;&#xff1b;&#x4eba;&#x5458;&#xff1a;&#x8d75;&#x5927;&#x54e5;&#xff0f;&#x6768;&#x9510;&#x5065;&#xff0f;&#x8bb8;&#x4eb2;&#x4eb2;&#xff0f;&#x8d75;&#x4f1f;&#xff0f;&#x676d;&#x5dde;&#x4eba;&#x5458;" ID="ID_274601685" CREATED="1575365231222" MODIFIED="1575365574852">
+<icon BUILTIN="group"/>
+<node TEXT="case" ID="ID_1464752982" CREATED="1575365588327" MODIFIED="1575365589162">
+<node TEXT="case 1:&#x591a;&#x4eba;Avatar" ID="ID_332763448" CREATED="1575365489817" MODIFIED="1575365502348"/>
+<node TEXT="case 2:&#x591a;&#x4eba;&#x51fa;&#x5883;&#x6446;pose&#x6216;&#x8005;&#x8df3;&#x821e;" ID="ID_300538261" CREATED="1575365502787" MODIFIED="1575365528359"/>
+<node TEXT="case 3:&#x771f;&#x4eba;&#x8138;&#x90e8;&#xff0f;&#x80a2;&#x4f53;&#x771f;&#x5b9e;&#x9a71;&#x52a8;" ID="ID_1845809874" CREATED="1575365529041" MODIFIED="1575365550475"/>
+</node>
+<node TEXT="Questions:" ID="ID_1794664119" CREATED="1575365596125" MODIFIED="1575365604200">
+<node TEXT="&#x591a;&#x4eba;avatar&#x8d44;&#x6e90;&#x592a;&#x591a;&#xff0c;&#x6297;&#x4e0d;&#x4f4f;" ID="ID_410671783" CREATED="1575365606529" MODIFIED="1575365640995"/>
+<node TEXT="&#x5b9e;&#x65f6;verify&#x6bd4;&#x8f83;&#x5403;&#x529b;" ID="ID_1656695744" CREATED="1575365619861" MODIFIED="1575365682903"/>
+<node TEXT="DynamicBone&#x662f;&#x4e2a;&#x7b80;&#x5355;&#x7684;&#x5934;&#x53d1;&#x7269;&#x7406;&#x76f8;&#x5173;&#x7ec4;&#x4ef6;" ID="ID_759297478" CREATED="1575365683449" MODIFIED="1575365720805"/>
+<node TEXT="&#x4e3b;Avatar(base Avatar)&#x5bf9;&#x5e94;&#x591a;feature &#x53ef;&#x80fd;&#x9700;&#x8981;&#x53d8;&#x6210; Avatar&#x4e0e;feature&#x591a;&#x5bf9;&#x591a;" ID="ID_1519641718" CREATED="1575365722942" MODIFIED="1575365780408"/>
+</node>
+</node>
+<node TEXT="st3dengine&#x4e3b;&#x8981;&#x7531;&#x676d;&#x5dde;&#x540c;&#x4e8b;&#x7ef4;&#x62a4;&#xff0c;&#x6211;&#x4eec;&#x8fd9;&#x8fb9;&#x9700;&#x8981;&#x66f4;&#x591a;&#x7684;&#x96c6;&#x4e2d;&#x5728;st_mobile&#x4e0b;" ID="ID_1731984032" CREATED="1575366058129" MODIFIED="1575366089955"/>
+<node TEXT="metaScene&#x5728;&#x5f15;&#x64ce;&#x4e2d;&#x7684;&#x5b9a;&#x4f4d;&#x8ba9;&#x6211;&#x611f;&#x89c9;&#x542c;&#x522b;&#x626d;&#xff01;" ID="ID_765643536" CREATED="1575373896722" MODIFIED="1575373926154">
+<icon BUILTIN="smily_bad"/>
+</node>
+</node>
+</node>
+<node TEXT="wednesday" POSITION="right" ID="ID_294536618" CREATED="1574819730332" MODIFIED="1575365125838">
+<edge COLOR="#ffff00"/>
+<node TEXT="waht to do" ID="ID_302156096" CREATED="1574819747292" MODIFIED="1574819749714">
+<node TEXT="&#x53bb;&#x4e0a;&#x6d77;&#x5f00;&#x5e74;&#x4f1a;&#x9020;&#x6210;&#xff18;&#x70b9;&#x534a;&#x7684;&#x98de;&#x673a;&#xff0c;&#x9884;&#x7ea6;&#xff16;&#x70b9;&#x7684;&#x4f01;&#x4e1a;&#x6ef4;&#x6ef4;&#xff0c;&#x4e00;&#x5171;&#xff11;&#xff16;&#xff10;&#x591a;&#x5143;&#xff08;&#x4e0e;&#x9ad8;&#x901f;&#x8d39;&#xff09;" ID="ID_546301159" CREATED="1575596780884" MODIFIED="1575596828705"/>
+</node>
+<node TEXT="what got" ID="ID_251103019" CREATED="1574819753370" MODIFIED="1574819755537">
+<node TEXT="&#x5546;&#x6c64;&#x5e74;&#x4f1a;&#x5f88;&#x8d5e;&#xff0c;&#x8001;&#x677f;&#x5e76;&#x6ca1;&#x6709;&#x8bf4;&#x592a;&#x591a;&#xff08;&#x751a;&#x81f3;&#x538b;&#x6839;&#x6ca1;&#x8bf4;&#xff09;&#x4eca;&#x5e74;&#x7684;&#x4e1a;&#x7ee9;&#x3002;&#x5927;&#x90e8;&#x5206;&#x56de;&#x5fc6;&#x4e0e;&#x590d;&#x76d8;" ID="ID_1097369817" CREATED="1575596915578" MODIFIED="1575596977462"/>
+<node TEXT="&#x8282;&#x76ee;&#x6ca1;&#x6709;&#x73b0;&#x4ee3;&#x52b2;&#x7206;&#x821e;&#x8e48;&#xff0c;&#x4e2d;&#x56fd;&#x98ce;&#x5f88;&#x5f3a;&#xff0c;&#x6c64;&#x8001;&#x5e08;&#x5f88;&#x5e7d;&#x9ed8;&#x3002;" ID="ID_475764006" CREATED="1575596977767" MODIFIED="1575597028792"/>
+<node TEXT="&#x665a;&#x4e0a;&#x5927;&#x5bb6;&#x73a9;&#x72fc;&#x4eba;&#x6740;&#xff0c;&#xff2b;&#xff34;&#xff36;&#xff08;&#x9ea6;&#x5f88;&#x70c2;&#xff0c;&#x6ca1;&#x51e0;&#x9996;&#x6b4c;&#xff09;&#xff0c;&#x5927;&#x90e8;&#x5206;&#x4eba;&#x53bb;&#x4e86;&#x5916;&#x6ee9;&#x3002;" ID="ID_690143771" CREATED="1575597029505" MODIFIED="1575597099096"/>
+</node>
+</node>
+<node TEXT="thursday" POSITION="left" ID="ID_1010765121" CREATED="1574906303274" MODIFIED="1575365147529">
+<edge COLOR="#7c0000"/>
+<node TEXT="what to do" ID="ID_1465803434" CREATED="1574906314139" MODIFIED="1574906321622">
+<node TEXT="&#x4e2d;&#x5348;12&#x70b9;&#x98de;&#x673a;&#xff0c;&#x5ef6;&#x673a;&#x5230;&#xff11;&#xff12;&#x70b9;&#xff14;&#xff10;&#x8d77;&#x98de;" ID="ID_1744677440" CREATED="1575596835757" MODIFIED="1575596871163"/>
+<node TEXT="&#x4e0b;&#x5348;&#xff13;&#x70b9;&#x591a;&#x5230;&#x9996;&#x90fd;&#x673a;&#x573a;&#xff0c;&#x4e0e;&#x540c;&#x4e8b;&#x4e00;&#x8def;&#x8d70;&#x6765;&#xff0c;&#x76f4;&#x63a5;&#x56de;&#x5bb6;" ID="ID_765023595" CREATED="1575596871469" MODIFIED="1575596907175"/>
+</node>
+<node TEXT="what got" ID="ID_730589860" CREATED="1574906322243" MODIFIED="1574906329756"/>
+</node>
+<node TEXT="friday" POSITION="left" ID="ID_1640021713" CREATED="1574993170782" MODIFIED="1574993174271">
+<edge COLOR="#00007c"/>
+<node TEXT="what to do" ID="ID_60953724" CREATED="1574993176079" MODIFIED="1574993180080">
+<node TEXT="&#x5b66;&#x4e60;&#x5e76;&#x7ed8;&#x5236;st_mobile&#x4e2d;&#x7684;avatar&#x76f8;&#x5173;&#x7c7b;&#x56fe;" ID="ID_1757657482" CREATED="1575597167483" MODIFIED="1575635072653">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node TEXT="what got" ID="ID_1010780520" CREATED="1574993180567" MODIFIED="1574993182301">
+<node TEXT="ST3DResourceManager&#x4e0d;&#x50cf;&#x4e2a;&#x8d44;&#x6e90;&#x7ba1;&#x7406;&#x5668;&#xff0c;&#x800c;&#x662f;&#x4e00;&#x4e2a;&#x5305;&#x4ee3;&#x7406;(package proxy)" ID="ID_1977140381" CREATED="1575614056856" MODIFIED="1575614090913"/>
+<node TEXT="&#x8fd9;&#x662f;&#x4e00;&#x4e2a;3dmetascene : android -&gt; assets -&gt; newEngine -&gt; fox2.zip" ID="ID_1012936620" CREATED="1575633800345" MODIFIED="1575633848598"/>
+<node ID="ID_217005260" CREATED="1575634865795" MODIFIED="1575634934166"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0000">ST3DResourceManager&#34429;&#28982;&#26159;&#20010;&#8220;&#36164;&#28304;&#31649;&#29702;&#22120;&#8220;&#65292;&#20294;&#20854;&#27809;&#26377;&#20849;&#20139;&#30340;&#25104;&#20221;&#65292;&#26159;&#24037;&#20316;&#30340;&#37325;&#28857;&#12290;</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
