ecf96e4574cee630ee37a361b5d4b7edeea75b08
Jerry Chaos <z_Spark@163.com>
2019-12-08 17:42:07 +0800

    modify

--- start.cmd
+++ start.cmd
@@ -1,1 +1,1 @@
-..\tiddlywiki ..\blog --listen host=192.168.199.163 port=1234 credentials=mycredentials.csv "readers=(anon)" "writers=(authenticated)"
+tiddlywiki ..\blog --listen credentials=mycredentials.csv "readers=(anon)" "writers=(authenticated)"
