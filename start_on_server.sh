5cde4e7e20d417de5ee0d1bd2d21beab0ce708e9
Jerry Chaos <z_spark@163.com>
2020-01-08 13:37:32 +0800

    modify config

--- start_on_server.sh
+++ start_on_server.sh
@@ -1,1 +1,1 @@
-tiddlywiki ../blog --listen host=111.229.246.109 port=8060 credentials=mycredentials.csv "readers=(anon)" "writers=(authenticated)"
+tiddlywiki ../blog --listen host="172.17.0.8" port="8080" credentials=mycredentials.csv "readers=(anon)" "writers=(authenticated)"
